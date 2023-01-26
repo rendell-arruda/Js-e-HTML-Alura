556d778d3b56c21824dd788a6d0620297910fe81
Rendell Arruda <rendellarruda3@gmail.com>
2023-01-26 08:30:34 -0300

    aula:reaproveitando codigo

--- manipulando-o-dom/-robotron-2000-projeto_inicial/js/main.js
+++ manipulando-o-dom/-robotron-2000-projeto_inicial/js/main.js
@@ -1,10 +1,20 @@
-const robrotron = document.querySelector('#robotron');
-robrotron.addEventListener('click', event => {
-  console.log(event);
+const subtrair = document.querySelector('#subtrair');
+const somar = document.querySelector('#somar');
+const braco = document.querySelector('#braco');
+
+const controle = document.querySelectorAll('.controle-ajuste');
+console.log(controle);
+
+somar.addEventListener('click', () => {
+  manipulaDados('somar');
 });
 
-function dizOi(nome) {
-  console.log('oi ' + nome);
-  console.log('bem vindo ao roborton');
+subtrair.addEventListener('click', () => {
+  manipulaDados('subtrair');
+});
+
+function manipulaDados(operacao) {
+  if (operacao === 'subtrair') {
+    braco.value = parseInt(braco.value) - 1;
+  } else braco.value = parseInt(braco.value) + 1;
 }
-dizOi('Pedro');
